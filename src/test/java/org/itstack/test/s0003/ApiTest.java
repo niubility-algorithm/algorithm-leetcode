44029aa2a78a9df1d8bff5ebac9e6c1654a54904
fuzhengwei <184172133@qq.com>
2020-03-18 14:23:33 +0800

    小傅哥 | update

---
+++ src/test/java/org/itstack/test/s0003/ApiTest.java
@@ -0,0 +1,59 @@
+package org.itstack.test.s0003;
+
+import org.itstack.algorithm.leetcode.s0003.Solution;
+import org.junit.Test;
+
+/**
+ * 公众号：bugstack虫洞栈 | 沉淀、分享、成长，让自己和他人都能有所收获！
+ * 博  客：http://bugstack.cn
+ * Create by 小傅哥 on @2020
+ */
+public class ApiTest {
+
+    @Test
+    public void test_lengthOfLongestSubstring_1() {
+
+        System.out.println("预期结果3 " + new Solution().lengthOfLongestSubstring_1("abcabcbb"));
+        System.out.println("预期结果2 " + new Solution().lengthOfLongestSubstring_1("abba"));
+        System.out.println("预期结果3 " + new Solution().lengthOfLongestSubstring_1("pwwkew"));
+        System.out.println("预期结果2 " + new Solution().lengthOfLongestSubstring_1("au"));
+        System.out.println("预期结果5 " + new Solution().lengthOfLongestSubstring_1("nfpdmpi"));
+
+    }
+
+    @Test
+    public void test_lengthOfLongestSubstring_2() {
+        System.out.println("预期结果3 " + new Solution().lengthOfLongestSubstring_2("abcabcbb"));
+        System.out.println("预期结果2 " + new Solution().lengthOfLongestSubstring_2("abba"));
+        System.out.println("预期结果3 " + new Solution().lengthOfLongestSubstring_2("pwwkew"));
+        System.out.println("预期结果2 " + new Solution().lengthOfLongestSubstring_2("au"));
+        System.out.println("预期结果5 " + new Solution().lengthOfLongestSubstring_2("nfpdmpi"));
+    }
+
+    @Test
+    public void test_lengthOfLongestSubstring_4() {
+        System.out.println("预期结果3 " + new Solution().lengthOfLongestSubstring_4("abcabcbb"));
+        System.out.println("预期结果2 " + new Solution().lengthOfLongestSubstring_4("abba"));
+        System.out.println("预期结果3 " + new Solution().lengthOfLongestSubstring_4("pwwkew"));
+        System.out.println("预期结果2 " + new Solution().lengthOfLongestSubstring_4("au"));
+        System.out.println("预期结果5 " + new Solution().lengthOfLongestSubstring_4("nfpdmpi"));
+    }
+
+    @Test
+    public void test_lengthOfLongestSubstring_5() {
+        System.out.println("预期结果3 " + new Solution().lengthOfLongestSubstring_5("abcabcbb"));
+        System.out.println("预期结果2 " + new Solution().lengthOfLongestSubstring_5("abba"));
+        System.out.println("预期结果3 " + new Solution().lengthOfLongestSubstring_5("pwwkew"));
+        System.out.println("预期结果2 " + new Solution().lengthOfLongestSubstring_5("au"));
+        System.out.println("预期结果5 " + new Solution().lengthOfLongestSubstring_5("nfpdmpi"));
+    }
+
+    @Test
+    public void t(){
+        String[] messages = {
+                "abcabcbb"
+        };
+        System.out.println(messages.length);
+    }
+
+}
